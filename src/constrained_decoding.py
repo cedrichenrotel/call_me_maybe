133183399ccf1304dec2bce3f23398921b893d8a
cedrichenrotel <cedric.henrotel@gmail.com>
2026-06-23 15:05:45 +0200

    toujours bloquer sur le typage  et c est tres chiant

--- src/constrained_decoding.py
+++ src/constrained_decoding.py
@@ -11,7 +11,8 @@ def filter_vocab_by_prefix(element: str, vocab: dict) -> list[int]:
     filter_score: list[int] = []
 
     for token_str, token_id in vocab.items():
-        # On nettoie les caractères d'espacement bizarres propres aux tokenizers (ex: Ġ ou █)
+        # On nettoie les caractères d'espacement bizarres propres aux
+        # tokenizers (ex: Ġ ou █)
         clean_token = token_str.replace('Ġ', ' ').replace(' ', ' ')
 
         # Est-ce que le token commence ce qu'il nous reste à écrire ?
@@ -31,6 +32,7 @@ def filter_list_str(prefix: str, elements: list[str]) -> list[str]:
 def keyword_search(json_str: str, word: str) -> str:
 
     prefix: list[str] = json_str.split(word)
+
     return prefix[-1]
 
 
@@ -47,8 +49,10 @@ def filter_score(elements: list[str], prefix: str, vocab: dict,
         rst.append(vocab.get('"'))
 
     for index, _ in enumerate(scores):
+
         if index not in rst:
             scores[index] = float('-inf')
+
     return scores
 
 
@@ -58,21 +62,18 @@ def selection_type(hint: str, vocab: dict, scores: list[float]) -> list[float]:
     if hint == 'boolean':
 
         response_list: list[str] = ['false', 'true']
-
-        score = filter_score(response_list, "", vocab, scores)
+        score: list[float] = filter_score(response_list, "", vocab, scores)
 
     elif hint == 'number':
 
         response_list: list[str] = [
             '-', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
             ]
-
         score = filter_score(response_list, "", vocab, scores)
 
     elif hint == 'string':
 
         response_list: list[str] = ['"']
-
         score = filter_score(response_list, "", vocab, scores)
 
     else:
@@ -109,12 +110,12 @@ def constrained_decoding(scores: list[float], json_tokens: list[int],
         if not function:
             raise ValueError('Function not found')
 
+        param_prefix: str = keyword_search(json_str, '"parameters": {')
+        param = param_prefix.split(',')[-1]
+
         # lister les dict de parameters
         list_keys: list[str] = [k for k in function.parameters.keys()]
 
-        param_prefix: str = keyword_search(json_str, '"parameters": {')
-
-        param = param_prefix.split(',')[-1]
         # gere le cas si '"' ouvrant de la clés est deja present
         if param.startswith(' "'):
 
@@ -122,23 +123,44 @@ def constrained_decoding(scores: list[float], json_tokens: list[int],
 
         # liste des cles de parameters non utiliser
         list_unused_keys: list[str] = ['"' + key for key in list_keys
-                                       if not f'{key}":' in json_str]
+                                       if not f'{key}":' in
+                                       param_prefix.replace(' ', '')]
+
+        # si la liste des list_unused est vide ferme l'acolade
+        if not list_unused_keys:
+            if param.split(':')[1].strip():
+                rst = [vocab.get('}')]
+                for index, _ in enumerate(scores):
+
+                    if index not in rst:
+                        scores[index] = float('-inf')
+                print(f'index: {index}')
+
+            return scores
 
         # liste de cles non utliser mais filtrer avec prefix
         keys_list: list[str] = filter_list_str(param, list_unused_keys)
 
         # recuperer la valeur dans la cles(type)
-        if '"' in param:
+        if (param.strip(' "') != "" and param.count('"') % 2 == 0 and
+           ':' not in param):
+            return scores
 
-            key_name: str = param.split(':')[0].strip('"')
+        elif ':' in param and '"' in param:
+            key_name: str = param.split(':')[0].strip(' "')
+            # print(f'key_name: {repr(key_name)}')
 
             if key_name in function.parameters:
-
                 param_type: str = function.parameters[key_name]["type"]
-                new_scores: list[float] = selection_type(param_type,
-                                                         vocab,
-                                                         scores)
-                return new_scores
+
+                if param_type == "string" and '"' in param:
+                    return scores
+
+                elif not param.split(':')[1].strip():
+                    new_scores: list[float] = selection_type(param_type,
+                                                             vocab,
+                                                             scores)
+                    return new_scores
 
             return scores
 
@@ -147,5 +169,5 @@ def constrained_decoding(scores: list[float], json_tokens: list[int],
     else:
 
         return scores
-
+    # print(json_str)
     return new_scores
